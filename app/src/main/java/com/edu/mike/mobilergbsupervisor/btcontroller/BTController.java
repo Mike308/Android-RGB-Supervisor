274dcf564d73908598a464b7c2f7804f1fa7d87e
Majkel44 <m.werner9205@gmail.com>
2017-05-08 18:13:53 +0200

    - added private field bluetoothAdapter - added private field bluetoothSocket - added private field isConnected for containing information about status connected (true - connected false - not connected) - added and implemented method for sending data through bluetooh

--- app/src/main/java/com/edu/mike/mobilergbsupervisor/btcontroller/BTController.java
+++ app/src/main/java/com/edu/mike/mobilergbsupervisor/btcontroller/BTController.java
@@ -1,8 +1,45 @@
 package com.edu.mike.mobilergbsupervisor.btcontroller;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.util.UUID;
+
 /**
  * Created by Mike on 2017-05-08.
  */
 
 public class BTController {
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothSocket bluetoothSocket;
+    private boolean isConnected;
+
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    public void sendBT(String str){
+
+        if (bluetoothSocket != null){
+
+            try{
+
+                bluetoothSocket.getOutputStream().write(str.getBytes());
+
+
+            }catch (IOException e){
+
+
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+
 }
