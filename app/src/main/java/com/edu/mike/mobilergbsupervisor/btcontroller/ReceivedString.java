217c10bc387fe6e16ce213f6a4979694f7228113
Majkel44 <m.werner9205@gmail.com>
2017-05-18 19:04:15 +0200

    - added class for presenting received stirng - added method for getting received string

---
+++ app/src/main/java/com/edu/mike/mobilergbsupervisor/btcontroller/ReceivedString.java
@@ -0,0 +1,20 @@
+package com.edu.mike.mobilergbsupervisor.btcontroller;
+
+/**
+ * Created by Mike on 2017-05-18.
+ */
+
+public class ReceivedString {
+
+    private String receivedString;
+
+    public ReceivedString(String receivedString) {
+        this.receivedString = receivedString;
+    }
+
+    public String getReceivedString() {
+        return receivedString;
+    }
+
+
+}
