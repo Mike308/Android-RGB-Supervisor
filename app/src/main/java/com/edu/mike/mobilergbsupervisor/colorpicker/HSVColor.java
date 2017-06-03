bd7625c3257a0b51f2e9d8aafc2f2f3559cf67b2
Majkel44 <m.werner9205@gmail.com>
2017-06-03 13:26:42 +0200

    - added class to contain hsv color structure for event bus

---
+++ app/src/main/java/com/edu/mike/mobilergbsupervisor/colorpicker/HSVColor.java
@@ -0,0 +1,35 @@
+package com.edu.mike.mobilergbsupervisor.colorpicker;
+
+/**
+ * Created by Mike on 2017-06-03.
+ */
+
+public class HSVColor {
+
+    private float h;
+    private float s;
+    private float v;
+
+    public HSVColor(float h, float s, float v){
+
+        this.h = h;
+        this.s = s;
+        this.v = v;
+
+
+    }
+
+    public float getH() {
+        return h;
+    }
+
+    public float getS() {
+        return s;
+    }
+
+    public float getV() {
+        return v;
+    }
+
+
+}
